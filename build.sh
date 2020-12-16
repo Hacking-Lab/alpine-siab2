a4c651527df8e8a25759502e58b8fce8874d5c60
Ivan Buetler <ivan.buetler@compass-security.com>
2020-12-16 12:22:20 +0100

    update

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
-docker build --no-cache -t hackinglab/alpine-siab -f Dockerfile .
+docker build --no-cache -t hackinglab/alpine-siab2:3.2.0 -t hackinglab/alpine-siab2:3.2 -t hackinglab/alpine-siab2:latest -f Dockerfile .
+
--- root/flag-deploy-scripts/deploy-file-flag.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/with-contenv bash
-
-echo "put your commands to deploy the file based flag here"
-echo "the /goldnugget/*.gn contains the flag"
-
