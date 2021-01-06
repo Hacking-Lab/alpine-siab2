e26800df905b79dd78160e7e9eafc8df55acd69a
Ivan Buetler <ivan.buetler@compass-security.com>
2021-01-06 07:21:03 +0100

    remove alpine-siab

--- README.md
+++ README.md
@@ -1,7 +1,2 @@
 # Alpine Linux Shell in a Box Service
 
-## Video2
-[![Video](img/alpine-siab2-video.png)](https://github.com/Hacking-Lab/alpine-siab2/blob/master/alpine-siab2.mp4)
-
-
-
--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 docker build --no-cache -t hackinglab/alpine-siab2:3.2.0 -t hackinglab/alpine-siab2:3.2 -t hackinglab/alpine-siab2:latest -f Dockerfile .
-docker build --no-cache -t hackinglab/alpine-siab:3.2.0 -t hackinglab/alpine-siab:3.2 -t hackinglab/alpine-siab:latest -f Dockerfile .
 
 docker push hackinglab/alpine-siab2
-docker push hackinglab/alpine-siab
 
