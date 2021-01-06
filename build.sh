c00b6f77e5bebd70fd995c6454c7e9ebbd989d80
Ivan Buetler <ivan.buetler@compass-security.com>
2021-01-06 12:41:37 +0100

    update

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 docker build --no-cache -t hackinglab/alpine-siab2:3.2.0 -t hackinglab/alpine-siab2:3.2 -t hackinglab/alpine-siab2:latest -f Dockerfile .
 
 docker push hackinglab/alpine-siab2
-
+docker push hackinglab/alpine-siab2:3.2
+docker push hackinglab/alpine-siab2:3.2.0
